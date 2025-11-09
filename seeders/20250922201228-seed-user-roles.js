6b58c3f92a7bbae03c5a17726164d4a5b64664ed
Mthi123 <s227342720@mandela.ac.za>
2025-11-09 12:13:05 +0200

    create user update

--- seeders/20250922201228-seed-user-roles.js
+++ seeders/20250922201228-seed-user-roles.js
@@ -1,17 +1,35 @@
-'use strict';
 
+'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
+    // Sequelize has a built-in option for this!
+    const options = {
+      identityInsert: true, // This tells Sequelize to handle IDENTITY_INSERT for MS SQL
+    };
 
-      await queryInterface.bulkInsert('UserRoles', [
-        {role_name: 'survivor'}, //id 1
-        {role_name: 'counsellor'}, //id 2
-        {role_name: 'admin'} //id 3
-      ]);
+    await queryInterface.bulkInsert(
+      'UserRoles',
+      [
+        {
+          //id: 1, // Explicitly set ID
+          role_name: 'survivor',
+        },
+        {
+          //id: 2, // Explicitly set ID
+          role_name: 'counsellor',
+        },
+        {
+          //id: 3, // Explicitly set ID
+          role_name: 'admin',
+        },
+      ],
+      options // Pass the options object here
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  down: async (queryInterface, Sequelize) => {
+    // The down method is fine
     await queryInterface.bulkDelete('UserRoles', null, {});
-  }
+  },
 };
